747ec8b42d7283056f358acbf8592dca241d3883
Mark Alatraca Jr. <mamalatraca@nemsu.edu.ph>
2025-06-12 00:52:50 +0800

    Update hnt_sintegration.cpp

--- src/hnt_sintegration.cpp
+++ src/hnt_sintegration.cpp
@@ -1,5 +1,10 @@
+/*
+  Configuration taken from open-source project
+  Sensor: DHT11 Humidity and Temperature Sensor
+*/
+
 #include "hnt_sintegration.h"
-#include <Arduino.h> // or #include <WProgram.h>
+#include <Arduino.h> 
 #include <dht11.h>
 
 
@@ -7,6 +12,7 @@ DHT11::DHT11(int pin) {
   sensorPin_second = pin;
 }
 
+//Call the following function to check if the sensor is working
 int DHT11::readhnt() {
   dht11 myDHT11;
   int chk = myDHT11.read(sensorPin_second);
@@ -30,12 +36,14 @@ int DHT11::readhnt() {
 
 }
 
+//read the humidity input
 double DHT11::humidity() {
     dht11 myDHT11;
     myDHT11.read(sensorPin_second);
     return ((float)myDHT11.humidity);
 }
 
+//read the temperature input
 double DHT11::temperature() {
     dht11 myDHT11;
     myDHT11.read(sensorPin_second);
@@ -43,16 +51,19 @@ double DHT11::temperature() {
 }
 
 
-
+//convert to fahr
 double DHT11::fahrenheit(double celsius) {
   return ((float)1.8 * celsius + 32);
 }
 
+//convert to kelvin
 double DHT11::kelvin(double celsius){
   return ((float)celsius + 273.15);
 }
 
 double DHT11::dewPoint(double celsius, double humidity){
+  // reference (1) : https://wahiduddin.net/calc/density_algorithms.htm
+  // reference (2) : https://www.colorado.edu/geography/weather_station/Geog_site/about.htm
   // (1) Saturation Vapor Pressure = ESGG(T)
   double RATIO = 373.15 / (273.15 + celsius);
   double RHS = -7.90298 * (RATIO - 1);
@@ -69,6 +80,9 @@ double DHT11::dewPoint(double celsius, double humidity){
   return ((float)(241.88 * T) / (17.558 - T));
 }
 
+// delta max = 0.6544 wrt dewPoint()
+// 6.9 x faster than dewPoint()
+// reference: https://en.wikipedia.org/wiki/Dew_point
 double DHT11::dewPointFast(double celsius, double humidity){
   double a = 17.271;
   double b = 237.7;
